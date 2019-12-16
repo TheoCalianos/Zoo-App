e3f51b40fde911210a339b9ced19b57b04939e12
Theo Calianos <43078567+TheoCalianos@users.noreply.github.com>
2019-12-16 16:04:40 -0500

    Initial commit

---
+++ Assets/Scripts/Mainmenu_Pannel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenu_Pannel : MonoBehaviour
+{
+  public Map_Pannel Map_Pannel;
+  public void ProcessInfo()
+  {
+    Map_Pannel.gameObject.SetActive(true);
+  }
+}
